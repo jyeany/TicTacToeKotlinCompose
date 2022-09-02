541aad7143898e8e700f38fff65e9fcc7eb2305d
jyeany <jyeany@gmail.com>
2022-09-01 22:46:21 -0600

    Change button colors when played

---
+++ src/jsMain/kotlin/components/board/BoardButton.kt
@@ -0,0 +1,42 @@
+package components.board
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Text
+import services.GameManager
+
+class BoardButton(private val gameManager: GameManager) {
+
+    private var spaceChar: Char by mutableStateOf('-')
+
+    @Composable
+    fun boardButton(i: Int, j: Int, updateFn: () -> Unit) {
+        val btnStyle = buttonStyle(spaceChar)
+        Button(
+            attrs = {
+                classes("mui-btn", btnStyle, "mui-btn--fab")
+                style { width(60.px) }
+                onClick {
+                    spaceChar = gameManager.playSquare(i, j)
+                    updateFn.invoke()
+                }
+            }
+        ) {
+            Text(spaceChar.toString())
+        }
+    }
+
+    private fun buttonStyle(c: Char): String {
+        return when (c) {
+            'X' -> "mui-btn--primary"
+            'O' -> "mui-btn--danger"
+            else -> "mui-btn--gray"
+        }
+    }
+
+}
--- src/jsMain/kotlin/components/board/TicTacToeDisplay.kt
+++ src/jsMain/kotlin/components/board/TicTacToeDisplay.kt
@@ -8,7 +8,7 @@ import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.width
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H1
+import org.jetbrains.compose.web.dom.H2
 import org.jetbrains.compose.web.dom.Text
 import services.GameManager
 
@@ -21,33 +21,16 @@ class TicTacToeDisplay(private val gameManager: GameManager) {
             currentMove = gameManager.currentPlayer
         }
         Div {
-            H1 {
+            H2 {
                 Text("Current Move: $currentMove")
             }
             for (i in 0 until GameManager.rows) {
                 Div {
                     for (j in 0 until GameManager.cols) {
-                        boardButton(i, j, update)
+                        BoardButton(gameManager).boardButton(i, j, update)
                     }
                 }
             }
-        }
-    }
-
-    @Composable
-    fun boardButton(i: Int, j: Int, updateFn: () -> Unit) {
-        var spaceChar: Char by mutableStateOf('-')
-        Button(
-            attrs = {
-                classes("mui-btn", "mui-btn--primary", "mui-btn--raised")
-                style { width(60.px) }
-                onClick {
-                    spaceChar = gameManager.playSquare(i, j)
-                    updateFn.invoke()
-                }
-            }
-        ) {
-            Text(spaceChar.toString())
         }
     }
 
